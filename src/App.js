d1af19f794f16b8f7c28dbc142d784c063c83f6f
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2022-01-03 15:07:07 +0900

    react app 생성, body 제목 태그 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='Title_body'>REACT를 이용한 메신저 프로젝트</h1>
     </div>
   );
 }
