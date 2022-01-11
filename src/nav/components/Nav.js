fc7601db0ae9458a1d98aeafe1be3dd1c2e5d9cd
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2022-01-11 22:01:37 +0900

    로그 아웃 기능 구현

--- src/nav/components/Nav.js
+++ src/nav/components/Nav.js
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Nav = (props) => {
+    const dispatch = useDispatch();
+
     const items = useSelector(props.getItems);
+    const logined = useSelector(props.getLogined);
+
     const list = items.map(item =>
         <li key={item.value}>
             <Link to={item.path}>{item.value}</Link>
@@ -11,6 +15,11 @@ const Nav = (props) => {
 
     return (
         <div className='Nav'>
+            {
+                logined
+                ? <button onClick={e => props.logout(dispatch)}>로그아웃</button>
+                : null
+            }
             <ul>{list}</ul>
         </div>
     )
--- src/nav/containers/Nav.js
+++ src/nav/containers/Nav.js
@@ -1,8 +1,11 @@
 import { connect } from 'react-redux'
 import Nav from '../components/Nav'
+import { toggleLoginState } from '../../article/articleSlice'
 
 export default connect((state) => {
     return {
-        getItems: state => state.nav.items
+        getItems: state => state.nav.items,
+        getLogined: state => state.article.logined,
+        logout: (dispatch) => dispatch(toggleLoginState())
     }
 })(Nav)
