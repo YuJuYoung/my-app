ec587f508cd54a54ba603136be696c23b160a2f8
YuJuYoung <51319610+YuJuYoung@users.noreply.github.com>
2022-01-13 18:28:10 +0900

    chatList 컴포넌트 내 p 태그에 key 요소 추가

--- src/article/chatList/components/ChatList.js
+++ src/article/chatList/components/ChatList.js
@@ -1,7 +1,7 @@
 const chatList = (props) => {
     const chats = props.chats;
 
-    const chatList = chats.map(chat => <p>{chat.title}</p>)
+    const chatList = chats.map(chat => <p key={chat.id}>{chat.title}</p>)
 
     return (
         <div className="chatList">
